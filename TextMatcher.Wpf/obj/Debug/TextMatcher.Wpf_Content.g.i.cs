968d81eaf8b176eb1e4817eb4040dff962a607de
michaelsync <mchlsync@gmail.com>
2016-01-18 01:58:40 +1100

    added the view and viewmodel and Ioc

--- TextMatcher.Wpf/obj/Debug/TextMatcher.Wpf_Content.g.i.cs
+++ TextMatcher.Wpf/obj/Debug/TextMatcher.Wpf_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("customdictionary.xml")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("themes/metro/howtoapplytheme.txt")]
 
 
